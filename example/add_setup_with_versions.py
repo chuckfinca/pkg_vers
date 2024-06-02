a86b3720f1acc19a7e6c6bc6c6cb133443547bb9
chuckfinca <chuckfinca@gmail.com>
2024-06-02 17:10:23 +0200

    version 0.0.1

---
+++ example/add_setup_with_versions.py
@@ -0,0 +1,14 @@
+import torch
+import numpy
+import matplotlib
+import pkg_vers
+
+project_folder_path = "./example/"
+
+# Get the .py file names in the project folder
+files_in_examples_folder = pkg_vers.find_all_py_files(project_folder_path)
+
+packages = pkg_vers.get_package_versions_from(files_in_examples_folder)
+
+for package, version in packages.items():
+    print(f"{package}: {version}")
---
+++ example/another_file_with_imports.py
@@ -0,0 +1,5 @@
+import graphviz
+import yaml
+import sklearn
+
+# ^ Adding more imports to gather for the demo
